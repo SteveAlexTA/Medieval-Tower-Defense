22f8ff9fd7c6b5ab8ee3750c2e650aabccf8ec20
SteveAlexTA <tuananhahihi2006@gmail.com>
2025-03-13 00:36:20 +0700

    Set up

--- Tower Defense Game SDL/Game.h
+++ Tower Defense Game SDL/Game.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL.h>
+#include <iostream>
+
+class Game {
+public:
+    Game();
+    virtual ~Game();
+
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+    bool running() { return isRunning; }
+
+private:
+    int cnt;
+    bool isRunning;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+#endif //GAME_H
+
--- Tower Defense Game SDL/main.cpp
+++ Tower Defense Game SDL/main.cpp
@@ -1,51 +1,22 @@
-//Using SDL and standard IO
 #include <SDL.h>
-#include <stdio.h>
+#include "Game.h"
 
-//Screen dimension constants
+Game* game = nullptr;
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-int main(int argc, char* args[])
-{
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+int main(int argc, char* argv[]) {
+    game = new Game();
+    game->init("TowerDefense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, true);
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
     }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
 
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-        }
-    } 
-    //Destroy window
-    SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
-    SDL_Quit();
-
+    game->clean();
+    delete game;  // Prevent memory leaks
     return 0;
 }
