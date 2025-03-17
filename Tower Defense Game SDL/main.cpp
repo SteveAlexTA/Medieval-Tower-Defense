e7c11407d31e8980a2153c1d0dc3c694639cc82a
SteveAlexTA <tuananhahihi2006@gmail.com>
2025-03-18 00:51:30 +0700

    Optimize

--- Tower Defense Game SDL/main.cpp
+++ Tower Defense Game SDL/main.cpp
@@ -7,13 +7,26 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 int main(int argc, char* argv[]) {
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    Uint32 frameStart;
+	int frameTime;
+
     game = new Game();
     game->init("TowerDefense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, true);
 
     while (game->running()) {
+        frameStart = SDL_GetTicks();
+        
         game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
     }
 
     game->clean();
